f9921feb434a92d4a5ef9fa4e904db80bca3351e
Anthony  Garcia <anthonygarcia@Anthonys-MacBook-Air.local>
2020-08-26 09:59:01 -0700

    initial xcode project creation

---
+++ youtubeapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtubeapp
+//
+//  Created by Anthony  Garcia on 8/26/20.
+//  Copyright © 2020 Anthony  Garcia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
